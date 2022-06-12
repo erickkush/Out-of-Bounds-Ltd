21200149e640a4e0728ff2a3dec8b4ce203da198
devkilyungi <victorkilyungi@gmail.com>
2022-06-12 20:52:45 +0300

    added PTS files

---
+++ PTSLibrary/DAO/SuperDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PTSLibrary.DAO
+{
+    class SuperDAO
+    {
+        protected Customer GetCustomer(int custId)
+        {
+            string sql;
+            SqlConnection cn;
+            SqlCommand cmd;
+            SqlDataReader dr;
+            Customer cust;
+
+            sql = "SELECT * FROM Customer WHERE CustomerId = " + custId;
+            cn = new SqlConnection("Server=localhost;Database=wm75;User Id=root;Password=abcd1234;");
+            cmd = new SqlCommand(sql, cn);
+
+            try
+            {
+                cn.Open();
+                dr = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                dr.Read();
+                cust = new Customer(dr["Name"].ToString(), (int)dr["CustomerId"]);
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Getting Customer", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return cust;
+        }
+
+    }
+}
---
+++ PTSLibrary/Project.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    class Project
+    {
+        private string name;
+        private DateTime expectedStartDate;
+        private DateTime expectedEndDate;
+        private Customer theCustomer;
+        private Guid projectId;
+        private List<Task> tasks;
+
+        public Project(string name, DateTime startDate, DateTime endDate, Guid projectId, Customer customer)
+        {
+            this.name = name;
+            this.expectedStartDate = startDate;
+            this.expectedEndDate = endDate;
+            this.projectId = projectId;
+            this.theCustomer = customer;
+        }
+
+        public Project(string name, DateTime startDate, DateTime endDate, Guid projectId, List<Task> tasks)
+        {
+            this.name = name;
+            this.expectedStartDate = startDate;
+            this.expectedEndDate = endDate;
+            this.projectId = projectId;
+            this.tasks = tasks;
+        }
+    }
+}
