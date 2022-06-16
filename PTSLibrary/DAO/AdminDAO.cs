ff356ff578b12a9b9f3b9006cbe86ac752a268ca
erickkush <76821303+erickkush@users.noreply.github.com>
2022-06-16 07:10:38 +0300

    Update AdminDAO.cs

--- PTSLibrary/DAO/AdminDAO.cs
+++ PTSLibrary/DAO/AdminDAO.cs
@@ -10,5 +10,29 @@ namespace PTSLibrary.DAO
 {
     class AdminDAO
     {
+        public void CreateProject(string name, DateTime startDate, DateTime endDate, int customerId, int administratorId)
+        {
+            string sql;
+            SqlConnection cn;
+            SqlCommand cmd;
+            Guid projectId = Guid.NewGuid();
+            sql = "INSERT INTO Project(ProjectId, Name, ExpectedStartDate, ExpectedEndDate, CustomerId, AdministratorId)";
+            sql += String.Format("VALUES({ 0},{ 1},{ 2},{ 3},{ 4},{ 5})", projectId, name, startDate, endDate, customerId, administratorId);
+            cn = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            cmd = new SqlCommand(sql, cn);
+            try
+            {
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Inserting", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
