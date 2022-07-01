6e433717e9b6ff24226972015086ce1d7ecef267
devkilyungi <victorkilyungi@gmail.com>
2022-07-01 12:05:39 +0800

    Update to file structure

---
+++ PTSLibrary/Facade objects/PTSSuperFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTSLibrary
+{
+	class PTSSuperFacade
+	{
+		protected DAO.SuperDAO dao;
+
+		public PTSSuperFacade(DAO.SuperDAO dao)
+        {
+			this.dao = dao;
+        }
+
+		public Task[] GetListOfTasks(Guid projectId)
+        {
+			return (dao.GetListOfTasks(projectId)).ToArray();
+        }
+	}
+}
